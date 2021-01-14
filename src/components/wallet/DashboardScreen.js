4aae3d204ac70dda10dedfb604d2af7169f02224
cedv1990 <cedv90@gmail.com>
2021-01-13 20:18:39 -0500

    first dashboard design

--- src/components/wallet/DashboardScreen.js
+++ src/components/wallet/DashboardScreen.js
@@ -1,11 +1,90 @@
 import React from 'react';
 
+const data = [
+    {
+        title: 'Balance Trend',
+        body: (
+            <>
+                <div className="summary">
+                    <span>TODAY</span>
+                    <p><b>0,00</b> <span>COP</span></p>
+                </div>
+
+                <div className="graph">
+
+                </div>
+            </>
+        )
+    },
+    {
+        title: 'Top expenses',
+        body: (
+            <>
+            </>
+        )
+    },
+    {
+        title: 'Last records',
+        body: (
+            <>
+            </>
+        )
+    }
+]
+
 export const DashboardScreen = () => {
     return (
         <>
-            <h3 className="wallet__body-title">Dashboard</h3>
-            <div className="wallet__body-content">
-                Contenido
+            <div className="wallet__dashboard-container">
+                <div className="wallet__dashboard-accounts">
+                    {
+                        ['#4dd0e1','#ab47bc','#4dd0e1','#ab47bc','#4dd0e1'].map(x => 
+                            (
+                                <div className="item">
+                                    <div className="icon" style={{ color: x }}>
+                                        <i className="fas fa-wallet"></i>
+                                    </div>
+                                    <div className="name">
+                                        Cash
+                                    </div>
+                                    <div className="value">
+                                        0,00
+                                    </div>
+                                    <div className="currency">
+                                        COP
+                                    </div>
+                                </div>
+                            )
+                        )
+                    }
+                    <div className="item new">
+                        <div className="icon">
+                            <i className="fas fa-plus-circle"></i>
+                        </div>
+                        <div className="text">
+                            Add account
+                        </div>
+                    </div>
+                    <div className="space"></div>
+                </div>
+
+                <div className="wallet__dashboard-cards-container">
+                    {
+                        data.map(({ title, body }) => (
+                            <div className="card__container">
+                                <h4>{ title }</h4>
+                                <hr />
+
+                                <div className="card__body">
+
+                                    { body }
+
+                                </div>
+                            </div>
+                        ))
+
+                    }
+                </div>
             </div>
         </>
     )
